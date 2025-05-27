76f0a874be9ce30d3afe8285aa2f43cac7bd5d59
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2025-05-27 15:12:20 -0300

    navbar resonsive y links interactivos

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import './Navbar.css';
+
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src='src/assets/logo.png' alt="Tantra Clásico de Argentina" />
+      </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+        <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+        <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+      </div>
+
+      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
+        <li><NavLink to="/prensa" onClick={closeMenu}>Prensa</NavLink></li>
+        <li><NavLink to="/contacto" onClick={closeMenu}>Contacto</NavLink></li>
+        <li><NavLink to="/internacional" onClick={closeMenu}>Internacional</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
